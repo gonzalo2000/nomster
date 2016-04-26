f5ef4358e404bbbf71215f20bbcea5a347ae8c72
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-04-26 02:02:08 +0000

    Adjust email settings (send to place creator)

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomster-gonzalo.com"
 
-  def comment_added
-    mail(to: "gonzalo2000@hotmail.com", subject: "Testing things out")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+
+    mail(to: @place_owner.email, subject: "A comment has been added to #{@place.name}")
   end
 end
